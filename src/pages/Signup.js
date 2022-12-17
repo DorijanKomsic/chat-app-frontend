ec31b47d87628df5b4ed4f82e415795ead730530
DorijanKomsic <komsic.dorijan@gmail.com>
2022-12-17 22:34:33 +0100

    Did some css for Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Container, Form, Button, Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import './Login.css'
+import './Signup.css'
 
 function Signup() {
   return (
     <Container>
       <Row className='justify-content-md-center'>
-        <Col md={5} className="login_bg"></Col>
+        <Col md={5} className="signup_bg"></Col>
         <Col md={7} className="d-flex align-items-center justify-content-ceneter flex-direction-column">
         <Form>
+          <h3>Please enter your credentials to create an account</h3>
+          <br />
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" />
