e01d77bf0c6f49c6fc5175c676a88f0c9a66e897
DorijanKomsic <komsic.dorijan@gmail.com>
2022-12-19 01:44:33 +0100

    connected the frontend with the backend

---
+++ src/features/userSlice.js
@@ -0,0 +1,28 @@
+import {createSlice} from '@reduxjs/toolkit';
+import appApi from '../services/appApi';
+
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: null,
+    reducers: {
+        addNotifications: (state, {payload}) => {
+
+        },
+        resetNotifications: (state, {payload}) => {
+
+        }
+    },
+
+    extraReducers: (builder) => {
+        // save user after signup
+        builder.addMatcher(appApi.endpoints.signupUser.matchFullfilled, (state, {payload}) => payload);
+        // save user after login
+        builder.addMatcher(appApi.endpoints.loginUser.matchFullfilled, (state, {payload}) => payload);
+        // destroy user session after logut
+        builder.addMatcher(appApi.endpoints.logoutUser.matchFullfilled, (state) => payload);
+    }
+})
+
+export const {addNotifications, resetNotifications} = userSlice.actions;
+export default userSlice.reducer;
